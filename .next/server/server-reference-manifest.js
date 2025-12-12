fe62f750775c626eaf1e6163ce7bf98114ce737d
Adelliaeka10 <adelliaeka10@gmail.com>
2025-12-12 09:32:53 +0700

    update1

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DCgEHCH+TFiew+jt7FMEZSUYiGRmwKmt3IX0UtASyik=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
